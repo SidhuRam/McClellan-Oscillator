a4e1d61daf08c89a5c0ff5c15646bf93d1efd583
Sidhu Ram <sidhu.lsk@gmail.com>
2020-02-05 10:58:57 +0530

    Update MO Extract.py

--- MO Extract.py
+++ MO Extract.py
@@ -1,24 +1,16 @@
-from bs4 import BeautifulSoup
+import requests
 import pandas as pd
-import requests
-
-
-url = "https://www1.nseindia.com/live_market/dynaContent/live_market.htm"
-webpage = requests.get(url);
-soup = BeautifulSoup(webpage.content, "html.parser");
-#soup.findAll('table')
-#advance = soup.find_all("td",attrs={"id":"advanceDecline - "})
-for tr in soup.find_all('tr'):
-    advance = tr.find_all('td')
-    print(advance)
-    
-#################################################################
-
-import requests
-
+from datetime import date
+from openpyxl import load_workbook
 url = 'https://www1.nseindia.com/live_market/dynaContent/live_analysis/changePercentage.json'
 r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 data = r.json()
 advance = data['rows'][0]['advances']
 decline = data['rows'][0]['declines']
 
+toappend = pd.DataFrame({'Advance':advance, 'Decline':decline}, index =[0]);
+toappend['Date'] = date.today()
+writer = pd.ExcelWriter('D:\Python Exercices\McClellanOscillator.xlsx', engine = 'openpyxl')
+toappend.to_excel(writer, index = False)
+toappend.to_excel(writer, startrow = curr_count + 1, index = False)
+writer.save();
